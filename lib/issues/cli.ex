8e902ff787597791de8d08f5a6f9d52a12f7a1ca
Nicholas Stevens <nichstev@gmail.com>
2025-11-30 19:51:15 +0100

    Add command line interface

--- lib/issues/cli.ex
+++ lib/issues/cli.ex
@@ -8,7 +8,22 @@ defmodule Issues.CLI do
   table of the last _n_ issues in a github project
   """
 
-  def run(argv), do: parse_args(argv)
+  def run(argv) do 
+    argv
+    |> parse_args()
+    |> process
+  end
+
+  def process(:help) do
+    IO.puts """
+    usage: issues <user> <project> [ count | #{@default_count} ]
+    """
+    System.halt(0)
+  end
+
+  def process({user, project, _count}) do
+    Issues.GithubIssues.fetch(user, project)
+  end
 
   @doc """
   `argv` can be -h of --help, which returns :help.
@@ -18,16 +33,21 @@ defmodule Issues.CLI do
   """
 
   def parse_args(argv) do
-    parse = OptionParser.parse(argv, switches: [ help: :boolean],
-                                     aliases:  [ h:    :help   ])
-    case parse do
-    {[help: true], _, _ }
-      -> :help
-    {_,  [user, project, count], _ }
-      -> {user, project, count}
-    {_,  [user, project], _ }
-      -> {user, project, @default_count}
-    _ -> :help
-    end
+    OptionParser.parse(argv, switches: [ help: :boolean],
+                             aliases:  [ h:    :help   ])
+    |> elem(1)
+    |> args_to_internal_representation()    
+  end
+
+  def args_to_internal_representation([user, project, count]) do
+    { user, project, String.to_integer(count) }
+  end
+
+  def args_to_internal_representation([user, project]) do
+    { user, project, @default_count }
+  end
+
+  def args_to_internal_representation(_) do # bad arg or --help
+    :help
   end
 end
